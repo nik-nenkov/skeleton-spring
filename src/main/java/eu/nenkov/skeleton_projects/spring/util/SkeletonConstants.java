f602a451aa719c8e8e3f2784b171244880b06047
Nikolay Nenkov <nik@nenkov.eu>
2024-01-16 15:39:56 +0200

    initial commit

---
+++ src/main/java/eu/nenkov/skeleton_projects/spring/util/SkeletonConstants.java
@@ -0,0 +1,9 @@
+package eu.nenkov.skeleton_projects.spring.util;
+
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class SkeletonConstants {
+    public static final String SOME_TEXT = "Greeting message for example";
+}
