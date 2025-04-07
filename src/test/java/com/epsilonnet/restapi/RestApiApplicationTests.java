37ef025affc09aa2629ac6832b5ddfab8dca55fd
Nik <nn8543@gmail.com>
2025-04-07 20:47:20 +0300

    trying with prod settings

--- src/test/java/com/epsilonnet/restapi/RestApiApplicationTests.java
+++ src/test/java/com/epsilonnet/restapi/RestApiApplicationTests.java
@@ -1,7 +1,7 @@
 package com.epsilonnet.restapi;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
 
 //@SpringBootTest
 class RestApiApplicationTests {
